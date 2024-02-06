c21020d3a8a6cdd092e5b84e2c3979e087211b2d
jktjkt15 <jktjkt15@jktjkt15.com>
2024-02-06 13:28:38 -0500

    fix(install): tweaked fish handling during install

--- repos/scripts/install.sh
+++ repos/scripts/install.sh
@@ -11,10 +11,10 @@ makepkg -si
 
 sudo pacman -S --needed --noconfirm cifs-utils dmenu dotnet-sdk exa fd freerdp fuzzel fzf git-delta github-cli hyprland hyprpaper inetutils jq lastpass-cli lxappearance mako man-db neofetch noto-fonts noto-fonts-emoji openfortivpn otf-font-awesome python-pip ripgrep tealdeer ttf-cascadia-mono-nerd waybar wget wireplumber wl-clipboard xfce4-settings yq
 
-yay -Syu --needed brave-bin discord elixir expressvpn firefox slack-desktop spotify vlc wezterm remmina-git dracula-gtk-theme dracula-icons-git cli-visualizer roon-tui spotify-tui swaylock-effects-git
+yay -Syu --needed --answerclean None --answerdiff None --cleanafter --removemake --answerupgrade brave-bin discord elixir expressvpn firefox slack-desktop spotify vlc wezterm remmina-git dracula-gtk-theme dracula-icons-git cli-visualizer roon-tui spotify-tui swaylock-effects-git
 
-echo '' >> .bashrc
-echo 'fish' >> .bashrc
-source .bashrc
+echo '' >> ~/.bashrc
+echo 'fish' >> ~/.bashrc
+source ~/.bashrc
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher update
